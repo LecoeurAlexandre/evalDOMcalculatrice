de193c765eaf8b1dad7e6194d0d615541f876467
Alexandre Lecoeur <alexandre.lecoeur@gmail.com>
2023-01-20 11:31:55 +0100

    pfff

---
+++ main.js
@@ -0,0 +1,23 @@
+let calc=0, affichage, getTouch;
+
+// Faire l'opération
+const calculate = () => {
+
+}
+
+// Donner le résultat de l'opération
+const result = () => {
+
+}
+
+// Récupérer le data type de la touche cliquée
+get1 = document.getElementById('1');
+get2 = document.getElementById('2');
+//console.log(get1)
+get1.addEventListener('click', () => {
+    console.log(get1.textContent)
+})
+
+get2.addEventListener('click', () => {
+    console.log(get2.textContent)
+})
